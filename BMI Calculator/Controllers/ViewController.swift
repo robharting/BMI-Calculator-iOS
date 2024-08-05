a4dee9c9d024d175223115367f37e28724aa052a
Rob Harting <robharting@hotmail.com>
2024-08-05 08:21:08 +0200

    added seconde view programmatically

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func calculateHeight(_ sender: UISlider) {
+        
+        let height = String(format: "%.2f", sender.value)
+        
+        heightLabel.text = "\(height)m"
+    }
+    
+    
+    @IBAction func calculateWeight(_ sender: UISlider) {
+    
+        let weight = String(format: "%.0f", sender.value)
+        
+        weightLabel.text = "\(weight)Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+   
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / pow(height, 2)
+              
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    
+    
 }
 
